7dde8cfd74036e0cc1ee6a9d5af79dd44bca38e3
k Raghavendra <kandular9@gmail.com>
2023-09-03 18:28:27 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What type of CSS is generally recommended for designing large web pages?",
+    answer: "External",
+    options: [
+      "Inline",
+      "Internal",
+      "External",
+      "None of these"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Which HTML tag is used to declare internal CSS?",
+    answer: "style tag",
+    options: [
+      "style tag",
+      "link tag ",
+      "script tag",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Can negative values be allowed in padding property?",
+    answer: "No",
+    options: [
+      "Yes",
+      "No",
+      "Depends on property",
+      "None of the above"
+    ]
+  },
+];
